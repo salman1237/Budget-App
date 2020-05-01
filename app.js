00d54755abd11e3664a804e18796eb6cf7597882
salman ahmed <you@example.com>
2020-05-01 18:12:50 +0600

    Button changed

--- app.js
+++ app.js
@@ -189,12 +189,12 @@ var UIController = (function () {
 
             if (type === 'inc') {
                 element=DOMStrings.incomeContainer
-                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><h6>Remove</h6></button></div></div></div>'
             }
             else if (type === 'exp') {
                 element = DOMStrings.expenseContainer
 
-                html = '<div div class="item clearfix" id = "exp-%id%" ><div     class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div >'
+                html = '<div div class="item clearfix" id = "exp-%id%" ><div     class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><h6>Remove</h6></button></div></div></div >'
             }
 
             // replace placeholder with some actual data
